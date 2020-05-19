021cb25d4c159b0ffe73da54944dd82792804eae
Sergei Starodubov <41199611+haxul@users.noreply.github.com>
2020-05-19 21:05:42 +0400

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello JENKINS');
+  res.end('update JENKINS');
 });
 
 server.listen(port, hostname, () => {
